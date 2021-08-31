f5c08d16f0a0259d34ff5195a5ea5332a238e67e
Sasha Kozlovskyy <alex33507@gmail.com>
2021-08-31 17:34:39 +0300

    Github search

--- src/components/api.js
+++ src/components/api.js
@@ -4,7 +4,7 @@ const BASE_URL = 'https://api.github.com';
 export const request = endpoint => fetch(`${BASE_URL}${endpoint}`, {
     method: 'GET',
     headers: {
-      Authorization: 'TOKEN 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',
+      Authorization: 'TOKEN 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',
     },
     })
   .then((response) => {
