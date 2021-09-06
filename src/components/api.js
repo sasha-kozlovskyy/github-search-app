845f9d61d6846960b704f1539ff318e0d0686032
Sasha Kozlovskyy <alex33507@gmail.com>
2021-09-06 16:05:57 +0300

    GitHub search

--- src/components/api.js
+++ src/components/api.js
@@ -2,10 +2,7 @@
 const BASE_URL = 'https://api.github.com';
 
 export const request = async(url) => {
-    const response = await fetch(`${BASE_URL}${url}`, {
-      method: 'GET',
-
-    });
+    const response = await fetch(`${BASE_URL}${url}`);
 
     if (!response.ok) {
       throw new Error(response.statusText);
